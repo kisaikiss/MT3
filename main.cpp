d85786b0c8a8fc6c95088a0efeacf6c365894287
kobayashi masato <k024g1031@g.neec.ac.jp>
2026-01-07 14:50:13 +0900

    MT4_01_02

--- main.cpp
+++ main.cpp
@@ -37,9 +37,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	char keys[256] = { 0 };
 	char preKeys[256] = { 0 };
 
-	std::shared_ptr<Camera> camera;
-	camera = std::make_shared<Camera>();
-
 	Vector3 from0 = Normalize(Vector3{ 1.0f, 0.7f, 0.5f });
 	Vector3 to0 = -from0;
 	Vector3 from1 = Normalize(Vector3{ -0.6f, 0.9f, 0.2f });
@@ -68,9 +65,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		
 #endif // _DEBUG
 
-		camera->Update(keys);
-	
-
 		///
 		/// ↑更新処理ここまで
 		///
@@ -79,7 +73,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 
-		DrawGrid(camera->GetVeiwProjectionMatrix(), camera->GetVeiwportMatrix());
 		MatrixScreenPrintf(0, 0, rotateMatrix0, "rotateMatrix0");
 		MatrixScreenPrintf(0, Define::kRowHeight * 5, rotateMatrix1, "rotateMatrix1");
 		MatrixScreenPrintf(0, Define::kRowHeight * 10, rotateMatrix2, "rotateMatrix2");
